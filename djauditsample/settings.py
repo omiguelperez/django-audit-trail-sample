311188b27e164ba7d65c4d00641f7712e2e54662
Oscar Pérez <mr.omiguelperez@gmail.com>
2020-05-30 21:55:06 -0500

    Add swagger

--- djauditsample/settings.py
+++ djauditsample/settings.py
@@ -40,6 +40,7 @@ DJANGO_APPS = [
 ]
 
 THIRD_PARTY_APPS = [
+    'drf_yasg',
     'audit_trail.apps.AuditTrailConfig',
 ]
 
--- djauditsample/urls.py
+++ djauditsample/urls.py
@@ -13,9 +13,41 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Django Audit Sample API",
+        default_version='v1',
+        description="Demo app using auth_trail",
+        contact=openapi.Contact(email="mr.omiguelperez@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Swagger urls
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'
+    ),
+    re_path(
+        r'^swagger/$',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+    re_path(
+        r'^redoc/$',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'
+    ),
 ]
